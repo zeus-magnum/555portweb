7fd31cd94133a3b50f71019aac0dc59bf4661806
zeus-magnum <60307093+zeus-magnum@users.noreply.github.com>
2020-04-13 21:28:48 +0530

    my portfolio website
    my portfolio website

---
+++ server.py
@@ -0,0 +1,40 @@
+
+from flask import Flask,render_template,url_for,request,redirect
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def  home_page():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def  html_page(page_name):
+    return render_template(page_name)
+
+#def write_to_file(data):
+    #with open('database.txt','a') as database:
+        #email = data["email"]
+        #subject = data["subject"]
+        #message = data["message"]
+       # file = database.write(f'\n{email},{subject},{message}')
+
+
+
+#def write_to_csv(data):
+    #with open('database.csv','a') as database2:
+        #email = data["email"]
+        #subject = data["subject"]
+        #message = data["message"]
+        #csv_writer = csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+       # csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method =='POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong.Try again!'
